4b50ab805891c420b117f8962392bff1c0301799
Imran Matin <imranmatin23@gmail.com>
2023-05-16 21:14:30 -0700

    Add comments to serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,21 @@
+"""
+Defines the Serializers for the API App. Serializers are used to convert Model objects into JSON.
+"""
+
 from rest_framework import serializers
 from .models import Room
 
 
 class RoomSerializer(serializers.ModelSerializer):
+    """
+    The RoomSerializer converts a Room Object into JSON.
+    """
+
     class Meta:
+        """
+        N/A
+        """
+
         model = Room
         fields = (
             "id",
@@ -16,14 +28,32 @@ class RoomSerializer(serializers.ModelSerializer):
 
 
 class CreateRoomSerializer(serializers.ModelSerializer):
+    """
+    The CreateRoomSerializer converts a Room Object into JSON specifically for the CreateRoom API.
+    """
+
     class Meta:
+        """
+        N/A
+        """
+
         model = Room
         fields = ("guest_can_pause", "votes_to_skip")
 
 
 class UpdateRoomSerializer(serializers.ModelSerializer):
+    """
+    The UpdateRoomSerializer converts a Room Object into JSON specifically for the Update API.
+    """
+
+    # NOTE: This is required due to the fact that code the serializer
+    # expects this to be a unique field in the database
     code = serializers.CharField(validators=[])
 
     class Meta:
+        """
+        N/A
+        """
+
         model = Room
         fields = ("guest_can_pause", "votes_to_skip", "code")
