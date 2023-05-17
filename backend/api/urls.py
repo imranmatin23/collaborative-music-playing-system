a11d83f65e28742259b13023b74864eb75c65c09
Imran Matin <imranmatin23@gmail.com>
2023-05-16 17:52:50 -0700

    Complete Tutorial 9

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoomView, JoinRoom
+from .views import RoomView, CreateRoomView, GetRoomView, JoinRoom, UserInRoom
 
 urlpatterns = [
     path("room", RoomView.as_view()),
     path("create-room", CreateRoomView.as_view()),
     path("get-room", GetRoomView.as_view()),
     path("join-room", JoinRoom.as_view()),
+    path("user-in-room", UserInRoom.as_view()),
 ]
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function HomePage() {
-  return <p>This is the home page</p>;
+  const navigate = useNavigate();
+
+  // NOTE: functional equivalent of async componentDidMount() function
+  useEffect(() => {
+    async function getUserInRoom() {
+      axios
+        .get("/api/user-in-room")
+        .then((response) => {
+          console.log(response);
+          if (response.data.code != null) {
+            navigate(`/room/${response.data.code}`);
+          }
+        })
+        .catch((error) => {
+          console.error("There was an error!", error);
+        });
+    }
+    getUserInRoom();
+  }, []);
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} align="center">
+        <Typography variant="h3" compact="h3">
+          House Party
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <ButtonGroup disableElevation variant="contained" color="primary">
+          <Button color="primary" to="/join" component={Link}>
+            Join a Room
+          </Button>
+          <Button color="secondary" to="/create" component={Link}>
+            Create a Room
+          </Button>
+        </ButtonGroup>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default HomePage;
