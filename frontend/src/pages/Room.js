299581658df6fb26a22ad836c69bf67070449548
Imran Matin <imranmatin23@gmail.com>
2023-05-16 22:05:58 -0700

    Add comments for Room page

--- frontend/src/pages/Room.js
+++ frontend/src/pages/Room.js
@@ -1,40 +1,55 @@
+/*
+ * This file defines the Room page.
+ */
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Grid, Button, Typography } from "@mui/material";
 import axios from "axios";
 import CreateRoomPage from "./CreateRoomPage";
 
+/*
+ * Render the Room component.
+ */
 function Room() {
+  // Initialize the state variables
   const [votesToSkip, setVotesToSkip] = useState(2);
   const [guestCanPause, setGuestCanPause] = useState(false);
   const [isHost, setIsHost] = useState(false);
   const [showSettings, setShowSettings] = useState(false);
 
+  // Set up navigation
   var navigate = useNavigate();
 
+  // Extract the URL parameters
   var params = useParams();
   const roomCode = params.roomCode;
 
+  // Define the function to get the details about a Room from the backend
   function getRoomDetails() {
     axios
       .get("/api/get-room?code=" + roomCode)
       .then((response) => response.data)
+      // Update the state with the Room's details
       .then((data) => {
         console.log(data);
         setVotesToSkip(data.votes_to_skip);
         setGuestCanPause(data.guest_can_pause);
         setIsHost(data.is_host);
       })
+      // Navigate to the Home Page if there are any errors
       .catch((error) => {
         console.error("There was an error!", error);
         navigate("/");
       });
   }
 
+  // Define the handler for when the Leave Button is pressed
   function leaveButtonPressed() {
+    // Invoke the LeaveRoom API to remove the user from the Room in the backend
     axios
       .post("/api/leave-room")
       .then((response) => response.data)
+      // Navigate to the Home Page after the backend API call completes
       .then((data) => {
         console.log(data);
         navigate("/");
@@ -44,13 +59,16 @@ function Room() {
       });
   }
 
+  // Define a function that updates the showSettings flag
   function updateShowSettings(value) {
     setShowSettings(value);
   }
 
+  // Return the Settings Button JSX
   function renderSettingsButton() {
     return (
       <Grid item xs={12} align={"center"}>
+        {/* When the settings button is clicked toggle the showSettings flag to true */}
         <Button
           variant="contained"
           color="primary"
@@ -62,10 +80,12 @@ function Room() {
     );
   }
 
+  // Return the Settings JSX
   function renderSettings() {
     return (
       <Grid container spacing={1}>
         <Grid item xs={12} align="center">
+          {/* Render the Create/Update Room Page with the current values */}
           <CreateRoomPage
             update={true}
             votesToSkip={votesToSkip}
@@ -75,6 +95,7 @@ function Room() {
           />
         </Grid>
         <Grid item xs={12} align="center">
+          {/* Toggle the showSettings flag to false when the Close button is clicked */}
           <Button
             variant="contained"
             color="secondary"
@@ -87,11 +108,13 @@ function Room() {
     );
   }
 
+  // Since there is no second argument specifiying any dependencies,
+  // this side effect is called after every render
   useEffect(() => {
     getRoomDetails();
   });
 
-  // Conditionally return the settings HTML
+  // If the ShowSettings flag is true then render the settings page
   if (showSettings) {
     return renderSettings();
   }
