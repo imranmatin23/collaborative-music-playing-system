00ba2d8f755fbee755d2ca012b10ae1d3aa0a788
Imran Matin <imranmatin23@gmail.com>
2023-05-16 21:42:14 -0700

    Add comments to frontend App.css, App.js, and index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,6 @@
+/*
+ * The App.js file represents your application or main component.
+ */
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreateRoomPage from "./pages/CreateRoomPage";
@@ -5,6 +8,11 @@ import RoomJoinPage from "./pages/RoomJoinPage";
 import Room from "./pages/Room";
 import "./App.css";
 
+/*
+ * The App function returns a BrowserRouter (Router) component that is able to declare individual routes within the application.
+ * The current browser URL will be matched against these routes and the matching route will be rendered.
+ * Ref: https://reactrouter.com/en/main/router-components/browser-router#browserrouter
+ */
 function App() {
   return (
     <div className="App">
