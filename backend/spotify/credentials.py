693ed8dd4bd946ea741f1f35e5a1a892c05abfba
Imran Matin <imranmatin23@gmail.com>
2023-05-19 12:16:37 -0700

    Remove sensitive credentials from Spotify

--- backend/spotify/credentials.py
+++ backend/spotify/credentials.py
@@ -3,7 +3,7 @@ Define the credentials for Spotify.
 """
 
 # TODO: Move to environment variables
-CLIENT_ID = "4f1dc3e38a55434e8d6e15ea0fa0fb48"
-CLIENT_SECRET = "2f2abc57ea464f278aa0124cd4026063"
+CLIENT_ID = "TODO"
+CLIENT_SECRET = "TODO"
 REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
 DNS_NAME = "http:/127.0.0.1:3000"
