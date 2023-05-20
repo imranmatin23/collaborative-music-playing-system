a6738cee5c66b1dcecf86047267624c104769a44
Imran Matin <imranmatin23@gmail.com>
2023-05-20 13:13:17 -0700

    Update Info pages with full instructions

--- frontend/src/pages/Info.js
+++ frontend/src/pages/Info.js
@@ -21,22 +21,30 @@ function Info(props) {
   const [page, setPage] = useState(pages.JOIN);
 
   // Define function to return the info on how the Join Page works
-  // TODO: Improve wording and content for Create page Info guide
   function joinInfo() {
-    return "Welcome to the guide for how to use the Join page!";
+    return (
+      '1/ Click "Join a Room".\n ' +
+      '2/ Enter a code for a Room and click "Enter Room".\n ' +
+      "3/ Depending on how the Host of the Room has configured the Room you can see the current song playing, pause/play it, and vote to skip it. If enough votes to skip the song are received, the song will be skipped!\n " +
+      '4/ Leave the Room by clicking the "Leave Room" button.'
+    );
   }
 
   // Define function to return the info on how the Create Page works
-  // TODO: Improve wording and content for Create page Info guide
   function createInfo() {
-    return "Welcome to the guide for how to use the Create page!";
+    return (
+      '1/ Click "Create a Room".\n ' +
+      '2/ Configure the settings for the Room and click "Create a Room".\n ' +
+      '3/ Click the "Settings" button to configure the settings for the room.\n ' +
+      '4/ Leave the Room by clicking the "Leave Room" button. This will close the room for all other users in the room.\n'
+    );
   }
 
   return (
     <Grid container spacing={1}>
       <Grid item xs={12} align="center">
         <Typography component="h4" variant="h4">
-          What is Collaborative Music Playing System?
+          {page === pages.CREATE ? "Create a Room Page" : "Join a Room Page"}
         </Typography>
       </Grid>
       <Grid item xs={12} align="center">
