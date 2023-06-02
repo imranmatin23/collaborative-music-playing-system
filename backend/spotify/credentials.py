60b03d566738ee43042fd8f91729e69cc6deb71d
Imran Matin <imranmatin23@gmail.com>
2023-06-01 17:14:15 -0700

    Convert Spotify credentials to environment variables

--- backend/spotify/credentials.py
+++
@@ -1,9 +0,0 @@
-"""
-Define the credentials for Spotify.
-"""
-
-# TODO: Move to environment variables
-CLIENT_ID = "TODO"
-CLIENT_SECRET = "TODO"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
-DNS_NAME = "http:/127.0.0.1:3000"
